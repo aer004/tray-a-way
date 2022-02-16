3f7f32733dfdeea9bd98f8a02baadeb1967e2507
hesther224 <78504149+hesther224@users.noreply.github.com>
2022-02-16 13:32:52 -0500

    Update tray.py

--- tray.py
+++ tray.py
@@ -3,3 +3,16 @@ def buzzer_on():
 def buzzer_off():
 
 def play_tone(tone):
+
+def readNFC():
+  # when the NFC scans, notify that it reads 
+  
+def greenLED():
+  # turn LED green if NFC was read successfully
+
+def redLED():
+  # else, turn LED red to notify user that the NFC was not read 
+
+def measureWeight():
+ 
+def detectWeightChange():
