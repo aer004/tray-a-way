0b47adae27abe0d1f2b44faf01d32e58be49d619
aer004 <ae004@gmail.com>
2022-02-15 19:22:16 -0500

    Added preliminary setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tray-a-way',
+	version='1.0',
+	description='Library for personal item monitoring device',
+	author='Bissell, Jung, Shapero',
+	author_email='brs424@nyu.edu, hej239@nyu.edu, rab762@nyu.edu',
+	url='https://github.com/rb2468/tray-a-way',
+	install_requires=[],
+	py_modules=[],
+	)
