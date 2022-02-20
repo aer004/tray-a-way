1b6b7d43cf2d6d7dcefa754244a89365abddd66b
rb2468 <rab762@nyu.edu>
2022-02-20 17:51:54 -0500

    Made changed to demo.py to play harry potter midi notes

--- demo.py
+++ demo.py
@@ -1,11 +1,10 @@
 import tray
 import time
 
-while (True == 1):
-	tray.buzzer_on()
-	print("Buzzer on")
-	time.sleep(5)
-	tray.buzzer_off()
-	print("Buzzer off")
-	time.sleep(5)
-
+harry_midi = [[988, 0.48], [1319, 0.72], [1568, 0.24], [1480, 0.48], [1319, 0.96], [1976, 0.48], [1760, 1.44]]
+for i in range(len(harry_midi)):
+	j = 0
+	tray.play_tone(harry_midi[i][j])
+	j += 1
+	time.sleep(harry_midi[i][j])
+tray.buzzer_off()
