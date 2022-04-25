db78abf0c337df1093f0080dce883e27dd6ec693
hesther224 <78504149+hesther224@users.noreply.github.com>
2022-04-25 13:47:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-/* 
+"""
 Issue: https://github.com/rb2468/tray-a-way/issues/72
+ -- import libraries
+ -- define an empty function for each task
  
 What we need:
 Continuously monitor if anything scanned
@@ -15,7 +17,7 @@ if armed: detect the weight
 if a specific amount of weight was changed then document it, and set off an alarm (true or false)
 else: no change
 Continuously monitor if anything scanned
-- changes armed vs. unarmed (true or false)
+- changes armed vs. disarmed (true or false)
 - if disarmed:
 - do nothing if there are weight changes
 
@@ -24,4 +26,23 @@ One thread constantly checking the state of the LED
 - armed (white)
 - disarmed (green)
 - failure to scan (red)
-*/
+"""
+
+import pigpio
+import time
+from py532lib.i2c import *
+from py532lib.frame import *
+from py532lib.constants import *
+from py532lib.mifare import *
+
+# Constants
+ARMED = True
+"""
+ARMED = True is the default since the Tray-a-way should be tracking for weight changes
+
+When ARMED = True, this means that  
+"""
+
+# Thread for continuously monitoring the NFC reader
+
+# Thread for checking if it is armed 
