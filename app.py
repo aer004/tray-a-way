b2ffa17ae692f436c37f26cc609e52be27dfd6d8
rb2468 <rab762@nyu.edu>
2022-05-02 14:01:14 -0400

    Added app1 to app

--- app.py
+++ app.py
@@ -29,6 +29,44 @@ WEIGHT_LOCK = Lock()
 #setup functions
 buzzer_setup()
 nfc_setup()
+led_setup()
+
+app = Flask(__name__, static_folder='assets')
+
+@app.route("/")
+def home():
+	return redirect("/templates/index")
+
+@app.route("/templates/index")
+def home_template():
+	return render_template("index.html")
+
+@app.route("/templates/alarm")
+def alarm_template():
+	arm = "disabled"
+	disarm = ""
+	if ARMED == True:
+		arm = ""
+		disarm = "disabled"
+	elif ARMED == False:
+		arm = "disabled"
+		disarm = ""
+	return render_template("alarm.html", armdisabled = arm, disarmdisabled = disarm)
+
+@app.route("/alarm/<int:action>")
+def alarm_act(action):
+	global ARMED
+	if action == 0:
+		ARMED = True
+	elif action == 1:
+		ARMED = False
+	return redirect("/templates/alarm")
+
+@app.route("/templates/logdata")
+def logdata_template():
+	d = [{'weight': 14, 'time': 'May 2 1:15PM'}]
+	return render_template("logdata", hist = d)
+
 
 
 # def check_armed():
@@ -68,6 +106,8 @@ t1 = Thread(target=check_armed)
 t1.start()
 t2 = Thread(target=check_weight)
 t2.start()
+
+app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
 
 t1.join()
 t2.join()
